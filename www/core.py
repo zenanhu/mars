9ae36db1733fa05868d5c087aabdaa48d4c14b82
Zenan <zenan2048@gmail.com>
2017-04-18 08:26:56 +0000

    add home router

--- www/core.py
+++ www/core.py
@@ -2,12 +2,19 @@
 
 from flask import Flask, request, Blueprint
 
+
 app = Flask(__name__)
 
 
 @app.route('/ping')
 def ping():
     return 'pong'
+
+
+#import mars.www.home
+@app.route('/')
+def home():
+    return '<h1>Hello</h1>'
 
 
 if __name__ == "__main__":
