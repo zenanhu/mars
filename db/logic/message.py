9a8a2b68704af79ef43dc12f96d42ab3636e0f59
Zenan <zenan2048@gmail.com>
2017-04-21 17:41:45 +0800

    add simple query

--- db/logic/message.py
+++ db/logic/message.py
@@ -1,3 +1,20 @@
 #!/user/bin/env python
 # coding: utf-8
 
+import MySQLdb
+
+def get_conn():
+    conn = MySQLdb.connect(user='space', passwd='space',
+                           host='localhost',
+                           db='mars')
+    return conn
+
+def get_messages():
+    conn = get_conn()
+    cursor = conn.cursor()
+    query = ("select * from Message")
+    cursor.execute(query)
+    print cursor.fetchone()
+
+    cursor.close()
+    conn.close()
