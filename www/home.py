17cb88d6340614bc268386a5194a930062d47a04
Zenan <zenan2048@gmail.com>
2017-04-22 00:54:14 +0800

    add input and list in home page

--- www/home.py
+++ www/home.py
@@ -5,18 +5,23 @@ from flask import render_template, make_response
 from mars.www.base import json_request, render_request
 from mars.www.core import app
 
+import mars.db.logic.message
+
 
 @app.route('/')
 @render_request('home.html')
 def home_test():
-    return {
+    messages = mars.db.logic.message.get_messages()
 
+    return {
+        'messages': messages
     }
 
 
 @app.route('/message', methods=['POST'])
 @json_request()
-def leave_message():
+def leave_message(message):
+    mars.db.logic.message.insert_message(message, message)
     return {
         'data': 'success',
     }
