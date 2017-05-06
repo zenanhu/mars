a5123dc56f1cc209f64558143975aa079d489cf7
Zenan <zenan2048@gmail.com>
2017-05-06 15:38:04 +0000

    change word url

--- www/word.py
+++ www/word.py
@@ -27,7 +27,7 @@ def dict_list(page=0):
     }
 
 
-@app.route('/dict/<word>', methods=['GET'])
+@app.route('/dict/word/<word>', methods=['GET'])
 @render_request('word.html')
 def search_dict(word):
     word = mars.db.logic.word.get_word(word)
