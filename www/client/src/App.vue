49d18d8ef6fd5beeac75f33fd44ac57d88a102b0
Zenan <zenan2048@gmail.com>
2017-10-21 16:01:34 +0000

    Add entry dictory

--- www/client/src/App.vue
+++ www/client/src/App.vue
@@ -15,6 +15,16 @@
                         <v-list-tile-title>Home</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
+
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>font_download</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title>Dictory</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
             </v-list>
         </v-navigation-drawer>
 
