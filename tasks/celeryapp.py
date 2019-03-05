be40aa1e0de904272097001a02e66b8e40f22798
Zenan <zenan2048@gmail.com>
2019-03-05 16:22:53 +0000

    <celery>fix import

--- tasks/celeryapp.py
+++ tasks/celeryapp.py
@@ -5,12 +5,12 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
 
-celery = Celery('mars_tasks',
+app = Celery('mars_tasks',
              broker='redis://localhost:6379/0',
              backend='redis://localhost:6379/0',
              include=['mars.tasks.test'])
 
 
 if __name__ == '__main__':
-    celery.start()
+    app.start()
 
--- tasks/test.py
+++ tasks/test.py
@@ -2,10 +2,10 @@
 # coding: utf-8
 
 from __future__ import absolute_import, unicode_literals
-from mars.tasks.celery import celery
+from mars.tasks.celeryapp import app
 
 
-@celery.task
+@app.task
 def add(x, y):
     return x + y
 
